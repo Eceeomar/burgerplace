72aa4849e16960a9d3341cfe3b2379467b00c186
unknown <isioma>
2023-06-06 23:42:40 +0100

    updated the codeds

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// import images as relative image path won't work with vite/vercel.
+import check from '../assets/check.svg'
+import star from '../assets/star.svg'
+import burger8 from '../assets/burger-8.png'
+import burger9 from '../assets/burger-9.png'
+import burger12 from '../assets/burger-12.png'
+
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -7,4 +14,45 @@ AOS.init({
     offset: 100,
 });
 
-// the additional code you saw in the video will not be needed :)
+const trendingBurgers = [
+    'Mushroom Swiss Burger',
+    'Make Burger',
+    'Bacon Burger',
+    'Breakfast Burger',
+    'Guacamole Burger',
+    'Classic Cheeseburger'
+];
+
+const trendingDrinks = [
+    "Sprite",
+    "Fanta",
+    "Schweppes",
+    "smoothie",
+    "Malt",
+    
+]
+
+const cards = [
+    {
+        imgSrc: sushi12,
+        alt: "sushi-12",
+        title: "Chezu Sushi",
+        rating: "4.8",
+        price: "$21.00"
+    },
+    {
+        imgSrc: sushi11,
+        alt: "sushi-11",
+        title: "Originale Sushi",
+        rating: "4.8",
+        price: "$21.00",
+        active: true
+    },
+    {
+        imgSrc: sushi10,
+        alt: "sushi-10",
+        title: "Ramen Legendo",
+        rating: "4.8",
+        price: "$21.00"
+    }
+];
